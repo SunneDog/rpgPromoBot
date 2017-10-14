daae9b8c78cf14cc8ab969753732c54c6947648c
Danni Sunne <etnichols55@gmail.com>
2017-10-13 21:43:33 -0700

    Tidyinig Structure

---
+++ PostArchive.py
@@ -0,0 +1,16 @@
+## rpgPromoBot ##
+# W/B: Danni Sunne
+# 10/13/17 ~ xx/xx/17
+## ----------- ##
+# This is where you put all the posts you want to
+# be able to queue from Main.py.
+## ----------- ##
+
+# Copy past this code while changing info to fit your post
+# Make sure the method name (ie examplePost()) is unique (there are no others with that name)
+
+## BEGIN COPY ##
+examplePost() : # name of the post's method to queue it.
+    postAlias = 'Alias' # This is what the program will refer to this post by
+                        # when running the queueing commands.
+    

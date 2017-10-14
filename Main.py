4cc6ba31a51729bf305e853d0fcd76f59f339cf9
Danni Sunne <etnichols55@gmail.com>
2017-10-13 20:54:54 -0700

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,7 @@
+## AOA PROMO BOT ##
+# W/B: Danni Sunne
+# 10/13/17 ~ xx/xx/17
+# Queues promo posts based on previously written +
+# created descriptions and graphics.
+# User commands bot to queue posts inserted into code.
+## ------------- ##

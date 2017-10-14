22f29c8eb394df79253b212d8035bf791f79fa26
Danni Sunne <etnichols55@gmail.com>
2017-10-13 21:44:34 -0700

    Tidying Structure

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
-## AOA PROMO BOT ##
+## rpgPromoBot ##
 # W/B: Danni Sunne
 # 10/13/17 ~ xx/xx/17
+## ----------- ##
 # Queues promo posts based on previously written +
 # created descriptions and graphics.
 # User commands bot to queue posts inserted into code.
-## ------------- ##
+## ----------- ##
+
+from PostArchive import ALLPOSTS
+
+
